421a0c67a007548bf263a4c494ec6d1b538afe7d
Jonathan Suh <hello@jonsuh.com>
2018-03-14 20:26:05 -0500

    Gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp        = require("gulp");
 var changed     = require("gulp-changed-in-place");
+var cssnano     = require("cssnano");
 var browserSync = require("browser-sync").create();
+var postcss     = require("gulp-postcss");
+var uglify      = require("gulp-uglify");
 
 gulp.task("watch", ["css"], function() {
   browserSync.init({
@@ -18,4 +21,25 @@ gulp.task("css", function() {
   return gulp.src("_site/assets/css/*.css")
     .pipe(changed())
     .pipe(browserSync.stream());
+});
+
+gulp.task("dist:css", function() {
+  return gulp.src("_site/assets/css/**/*.css")
+    .pipe(postcss([
+      cssnano()
+    ]))
+    .pipe(gulp.dest("_site/assets/css"));
+});
+
+gulp.task("dist:js", function() {
+  return gulp.src([
+      "assets/js/**/*.js",
+    ])
+    .pipe(uglify())
+    .pipe(gulp.dest("_site/assets/js"));
+});
+
+gulp.task("dist", function() {
+  gulp.start("dist:css");
+  gulp.start("dist:js");
 });
